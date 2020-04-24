823dd04b1cd35a4df892d30798ef207565c5ac68
Redseb <dwarfslikepie@gmail.com>
2020-04-24 19:36:56 +0200

    Updates

---
+++ precache-manifest.bc06e3775a2540ad008a01f364909a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb39e5763a117b7129df565fa7538e86",
+    "url": "/civ5Randomizer/index.html"
+  },
+  {
+    "revision": "6c36c9336b18c569b816",
+    "url": "/civ5Randomizer/static/css/main.3da22a4b.chunk.css"
+  },
+  {
+    "revision": "21f48e0119550cd14144",
+    "url": "/civ5Randomizer/static/js/2.315a8bca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/civ5Randomizer/static/js/2.315a8bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c36c9336b18c569b816",
+    "url": "/civ5Randomizer/static/js/main.9caf5fc2.chunk.js"
+  },
+  {
+    "revision": "9317f0b86a6e58e54ea9",
+    "url": "/civ5Randomizer/static/js/runtime-main.0e438489.js"
+  }
+]);
